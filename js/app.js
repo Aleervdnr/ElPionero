04363e68dfd487c2544c2d745e233405e19c2902
Aleervdnr <alejonahuelrott@gmail.com>
2021-09-29 12:06:28 -0300

    nav active on scroll

--- js/app.js
+++ js/app.js
@@ -11,5 +11,29 @@ const navSlide = () =>{
 }
 navSlide()
 
+// nav section active on scroll
+
+const sections = document.querySelectorAll("section")
+const navlink = document.querySelectorAll("nav ul li a")
+
+window.addEventListener("scroll", ()=>{
+  let current = ""
+  sections.forEach( section =>{
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.clientHeight
+    if(scrollY >= sectionTop - sectionHeight/3){
+      current = section.getAttribute("id")
+    }
+  })
+
+  navlink.forEach( a =>{
+    a.classList.remove("active")
+    if(a.classList.contains(current+"__link")){
+      a.classList.add("active")
+    }
+  })
+  console.log(current)
+})
+
 
 
